34c20f2f75525036294f93505b593de8bfab74c0
Alexandra Spillane <alexandra@spillane.me>
2019-10-28 11:55:08 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# aws_key_gen ZSH plugin
+
+Provides `_aws_profile` function to print out the currently active AWS profile.  If the token for that profile has
+expired, the text will be red, otherwise, orange.
---
+++ aws_key_gen.plugin.zsh
@@ -0,0 +1,8 @@
+# AWS profile
+function _aws_profile() {
+  local _aws_profile_color="166"
+  if ! aws_key_gen status &>/dev/null; then
+    _aws_profile_color="red"
+  fi
+  echo -n "${_black}aws${_colon}%{%F{${_aws_profile_color}}%B%}${AWS_PROFILE:-default}"
+}
